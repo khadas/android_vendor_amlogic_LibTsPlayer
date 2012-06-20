e75cc4d1f690467b5a5373a2bd6a61ffeb9c7d00
wenbiao.zhang <wenbiao.zhang@amlogic.com>
2012-06-20 16:58:05 +0800

    support TSPlayer for IPTV

--- TsPlayer.cpp
+++ TsPlayer.cpp
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "player.h"
 //#include "../IPTVPlayer/PubAndroid.h"	 
 #define DPrint(x)
 
@@ -52,8 +53,8 @@ int RmDefPath(void)
 {
 
     int fd, ret;
-    char str[]="rm all";
-    //char str[]="rm default";
+    //char str[]="rm all";
+    char str[]="rm default";
    // char str[] = "iptvpath";
     fd = open("/sys/class/vfm/map", O_RDWR);
 
@@ -205,9 +206,197 @@ int SYS_set_video_preview_win(int x,int y,int w,int h)
 	  return -1;
   }
 
+void get_display_mode(char *mode)
+{
+    int fd;
+    char *path = "/sys/class/display/mode";
+    if (!mode) {
+       // log_error("[get_display_mode]Invalide parameter!");
+        return;
+    }
+    fd = open(path, O_RDONLY);
+    if (fd >= 0) {
+        memset(mode, 0, 16); // clean buffer and read 15 byte to avoid strlen > 15	
+        read(fd, mode, 15);
+        //log_print("[get_display_mode]mode=%s strlen=%d\n", mode, strlen(mode));
+        mode[strlen(mode)] = '\0';
+        close(fd);
+    } else {
+        sprintf(mode, "%s", "fail");
+    };
+    //log_print("[get_display_mode]display_mode=%s\n", mode);
+    return ;
+}
+
+
+int GL_2X_iptv_scale720(int mSwitch)
+{
+
+	LOGI("GL_2X_iptv_scale720");
+  char mode[16];
+	char m1080scale[8];
+	int request2XScaleFile = -1, scaleOsd1File = -1, scaleaxisOsd1File = -1, Fb0Blank = -1, Fb1Blank = -1;
+	char raxis_str[32],saxis_str[32];
+	
+  
+	get_display_mode(mode);
+
+  
+	if((request2XScaleFile = open("/sys/class/graphics/fb0/request2XScale", O_RDWR)) < 0) {
+		//log_print("open /sys/class/graphics/fb0/scale fail.");
+	}
+	if((scaleOsd1File = open("/sys/class/graphics/fb1/scale", O_RDWR)) < 0) {
+		///log_print("open /sys/class/graphics/fb1/scale fail.");
+	}
+	if((scaleaxisOsd1File = open("/sys/class/graphics/fb1/scale_axis", O_RDWR)) < 0) {
+		//log_print("open /sys/class/graphics/fb1/scale_axis fail.");
+	}
+	if((Fb0Blank = open("/sys/class/graphics/fb0/blank", O_RDWR)) < 0) {
+		//log_print("open /sys/class/graphics/fb0/blank fail.");
+	}
+	if((Fb1Blank = open("/sys/class/graphics/fb1/blank", O_RDWR)) < 0) {
+		//log_print("open /sys/class/graphics/fb1/blank fail.");
+	}
+	if(mSwitch == 0)
+	{
+			write(request2XScaleFile, "2", strlen("2"));
+			write(scaleOsd1File, "0", strlen("0"));
+	}
+	else if(mSwitch == 1)
+	{
+		write(Fb0Blank, "1", strlen("1"));
+		//write(Fb1Blank, "1", strlen("1"));
+		if(!strncmp(mode, "480i", 4) || !strncmp(mode, "480p", 4))
+		{
+			write(request2XScaleFile, "16 720 480", strlen("16 720 480"));
+			write(scaleaxisOsd1File, "1280 720 720 480", strlen("1280 720 720 480"));
+			write(scaleOsd1File, "0x10001", strlen("0x10001"));
+		}
+		else if(!strncmp(mode, "576i", 4) || !strncmp(mode, "576p", 4))
+		{
+			write(request2XScaleFile, "16 720 576", strlen("16 720 576"));
+			write(scaleaxisOsd1File, "1280 720 720 576", strlen("1280 720 720 576"));
+			write(scaleOsd1File, "0x10001", strlen("0x10001"));
+		}
+		else if(!strncmp(mode, "720p", 4))
+		{
+			write(request2XScaleFile, "16 1280 720", strlen("16 1280 720"));	//for setting blank to 0
+		}
+		else if(!strncmp(mode, "1080i", 5) || !strncmp(mode, "1080p", 5))
+		{
+			write(request2XScaleFile, "8 0 0", strlen("8 0 0"));
+			write(scaleaxisOsd1File, "1280 720 1920 1080", strlen("1280 720 1920 1080"));
+			write(scaleOsd1File, "0x10001", strlen("0x10001"));
+		}
+	}
+
+	if(request2XScaleFile >= 0)
+		close(request2XScaleFile);
+	if(scaleOsd1File >= 0)
+		close(scaleOsd1File);
+	if(scaleaxisOsd1File >= 0)
+		close(scaleaxisOsd1File);
+	if(Fb0Blank >= 0)
+		close(Fb0Blank);
+	if(Fb1Blank >= 0)
+		close(Fb1Blank);
+	return 0;
+}
+
+
+int GL_2X_iptv_scale530(int mSwitch)
+{
+  char mode[16];
+	char m1080scale[8];
+	int request2XScaleFile = -1, scaleOsd1File = -1, scaleaxisOsd1File = -1, Fb0Blank = -1, Fb1Blank = -1;
+	char raxis_str[32],saxis_str[32];
+	
+	get_display_mode(mode);
+	
+	
+	
+	
+	LOGI("GL_2X_iptv_scale530");
+  
+	if((request2XScaleFile = open("/sys/class/graphics/fb0/request2XScale", O_RDWR)) < 0) {
+		//log_print("open /sys/class/graphics/fb0/scale fail.");
+	}
+	if((scaleOsd1File = open("/sys/class/graphics/fb1/scale", O_RDWR)) < 0) {
+		//log_print("open /sys/class/graphics/fb1/scale fail.");
+	}
+	if((scaleaxisOsd1File = open("/sys/class/graphics/fb1/scale_axis", O_RDWR)) < 0) {
+		//log_print("open /sys/class/graphics/fb1/scale_axis fail.");
+	}
+	
+	if((Fb0Blank = open("/sys/class/graphics/fb0/blank", O_RDWR)) < 0) {
+		//log_print("open /sys/class/graphics/fb0/blank fail.");
+	}
+	if((Fb1Blank = open("/sys/class/graphics/fb1/blank", O_RDWR)) < 0) {
+		//log_print("open /sys/class/graphics/fb1/blank fail.");
+	}
+	if(mSwitch == 0)
+	{
+			write(request2XScaleFile, "2", strlen("2"));
+			write(scaleOsd1File, "0", strlen("0"));
+			//write(scaleOsd0File, "0", strlen("0"));
+	}
+	else if(mSwitch == 1)
+	{
+	   //640, 530
+		write(Fb0Blank, "1", strlen("1"));
+		//write(Fb1Blank, "1", strlen("1"));
+		if(!strncmp(mode, "480i", 4) || !strncmp(mode, "480p", 4))
+		{
+			write(request2XScaleFile, "16 1440 652", strlen("16 1440 652"));
+			write(scaleaxisOsd1File, "1280 720 720 480", strlen("1280 720 720 480"));
+			write(scaleOsd1File, "0x10001", strlen("0x10001"));
+		}
+		else if(!strncmp(mode, "576i", 4) || !strncmp(mode, "576p", 4))
+		{
+			write(request2XScaleFile, "16 1440 782", strlen("16 1440 782"));
+			write(scaleaxisOsd1File, "1280 720 720 576", strlen("1280 720 720 576"));
+			write(scaleOsd1File, "0x10001", strlen("0x10001"));
+		}
+		else if(!strncmp(mode, "720p", 4))
+		{
+			write(request2XScaleFile, "16 2560 978", strlen("16 2560 978"));	//for setting blank to 0
+		}
+		else if(!strncmp(mode, "1080i", 5) || !strncmp(mode, "1080p", 5))
+		{
+			//write(request2XScaleFile, "8", strlen("8"));
+			write(request2XScaleFile, "8 1920 1467", strlen("8 1920 1467"));
+			//write(request2XScaleFile, "16 3840 1467", strlen("16 3840 1467"));
+			write(scaleaxisOsd1File, "1280 720 1920 1080", strlen("1280 720 1920 1080"));
+			write(scaleOsd1File, "0x10001", strlen("0x10001"));
+
+			//write(scaleaxisOsd0File, "0 0 959 1079", strlen("0 0 959 1079"));
+			//write(scaleOsd0File, "0x10000", strlen("0x10000"));
+			
+		}
+	}
+
+	if(request2XScaleFile >= 0)
+		close(request2XScaleFile);
+	if(scaleOsd1File >= 0)
+		close(scaleOsd1File);
+	if(scaleaxisOsd1File >= 0)
+		close(scaleaxisOsd1File);
+	/*if(scaleOsd0File >= 0)
+		close(scaleOsd0File);
+	if(scaleaxisOsd0File >= 0)
+		close(scaleaxisOsd0File);*/
+	if(Fb0Blank >= 0)
+		close(Fb0Blank);
+	if(Fb1Blank >= 0)
+		close(Fb1Blank);
+	return 0;
+}
 
 int enable_gl_2xscale(const char *val)
 {
+
+	return 0;
+
     int fd;
     int bytes;
 
@@ -259,13 +448,13 @@ int Active_osd_viewport(int web_w, int web_h)
 		  modes_width = 1280 ;
 
 		 }
-		 else if(!strncmp(buf,"1080p",5)){		
+		 else if(!strncmp(buf,"1080p50hz",9)){		
 
 		 modes_heigh = 1080;
 		 modes_width = 1920;
 
 		 }
-		 else if(!strncmp(buf,"lvds1080p",9)){	
+		 else if(!strncmp(buf,"1080i50hz",9)){	
 		 modes_heigh = 1080;
 		 modes_width = 1920;
 
@@ -280,19 +469,19 @@ int Active_osd_viewport(int web_w, int web_h)
 		 else if(!strncmp(buf,"576i",4))
 		 {
 		 modes_heigh = 576;
-		 modes_width = 1024;  
+		 modes_width = 720;  
 
 		 }
 		 else if(!strncmp(buf,"480p",4))
 		 {
 		  modes_heigh = 480;
-		  modes_width = 854;
+		  modes_width = 720;
 
 		 }
 		 else if(!strncmp(buf,"480i",4))
 		 {
 			 modes_heigh = 480;
-			 modes_width = 854;
+			 modes_width = 720;
 
 		 }
 		 close(fd_disp);     
@@ -300,12 +489,14 @@ int Active_osd_viewport(int web_w, int web_h)
 	 
 
 	 LOGI("gjaoun : mode %d , %d",modes_width,modes_heigh);
-
-	 int w = (modes_width*modes_width)/web_w ;
-     int h = (modes_heigh*modes_heigh)/web_h ;
+     sprintf(bcmd,"%d %d %d %d %d",0,0,modes_width,modes_heigh,0);
+     set_sys_str("/sys/class/graphics/fb0/video_hole",bcmd);	
+   //  Active_video_viewport(0,0,modes_width,modes_heigh);
+	 //int w = (modes_width*modes_width)/web_w ;
+     //int h = (modes_heigh*modes_heigh)/web_h ;
 	
-	
-	sprintf(bcmd,"%d %d %d",16,w,h);
+	memset(bcmd,0,sizeof(bcmd));
+	sprintf(bcmd,"%d %d %d",16,modes_width,modes_heigh);
 	LOGI("gjaoun scale: %s",bcmd);
 	enable_gl_2xscale(bcmd); 
 
@@ -337,7 +528,17 @@ int Active_video_viewport(int x,int y,int width,int height)
 
 void LunchIptv()
 {
+	
+		int ret;
 
+   ret = disable_freescale_MBX();
+		  LOGI("disable freeacale:%d\n", ret);
+	  
+    ret = GL_2X_iptv_scale720(1);
+   LOGI("GL2XScale:%d\n", ret);
+    
+    
+    set_sys_int("/sys/class/graphics/fb0/free_scale",0);
     RmDefPath();
     AddIptvPath();
 	set_sys_int("/sys/class/video/blackout_policy",0);
@@ -345,9 +546,13 @@ void LunchIptv()
     set_sys_int("/sys/module/di/parameters/buf_mgr_mode",0);
     set_sys_str("/sys/class/display/rd_reg","m 0x1a2b");
     set_sys_str("/sys/class/display/wr_reg","m 0x1a2b 0x1dc20c81");
-    set_sys_str("/sys/class/graphics/fb0/video_hole","0 0 1920 1080 2");	
-    Active_video_viewport(0,0,1920,1080);
-	Active_osd_viewport(1280, 720);
+	
+    set_sys_str("/sys/class/graphics/fb0/video_hole","0 0 1920 1080 1 8");	
+    //Active_video_viewport(0,0,1280,720);
+    set_sys_int("/sys/class/video/blackout_policy",0);
+    //Active_osd_viewport(1280, 720);
+	
+	
 
 }
 
@@ -364,10 +569,11 @@ void SwitchResolution(int mode , int status)
 		if(status == 0){	
           
 			//RemoveIptvPath();
-            Active_osd_viewport(640, 530);
+           //Active_osd_viewport(640, 530);
+             GL_2X_iptv_scale530(1);
 		  //  AddIptvPath();
            }
-		 Active_video_viewport(0,0,1920,1080);	
+		// Active_video_viewport(0,0,1920,1080);	
 		
     }
 
@@ -375,11 +581,14 @@ void SwitchResolution(int mode , int status)
 		if(status == 0){
          
 			//RemoveIptvPath();
-            Active_osd_viewport(1280, 720);
+          // Active_osd_viewport(1280, 720);
+          
+           GL_2X_iptv_scale720(1);
+		   
 		  // AddIptvPath();
 		
 		}
-		 Active_video_viewport(0,0,1920,1080);			
+	 // Active_video_viewport(0,0,1920,1080);			
       }
 	
 
@@ -391,12 +600,28 @@ void QuitIptv()
 {
 
 
-    RmDefPath();
+    //enable_gl_2xscale("");
+		int ret;
+		    ret = GL_2X_iptv_scale720(0);
+			GL_2X_iptv_scale530(0);
+
+    ret = enable_freescale_MBX();
+
+     LOGI("enableFreescaleMBX:%d\n", ret);
+	
+    //RmDefPath();
+    RemoveIptvPath();
     RestoreDefPath();
+	enable_gl_2xscale("2 0 0");
     set_sys_str("/sys/class/graphics/fb0/video_hole","0 0 0 0 0");
 	set_sys_int("/sys/class/video/blackout_policy",1);
-    enable_gl_2xscale("2");
-	enable_gl_2xscale("");
+    set_sys_int("/sys/class/graphics/fb0/free_scale",1);
+	
+	/*enable freescale for ui and disable opengl scale*/
+
+
+		               
+   
 }
 
 
@@ -488,25 +713,81 @@ int  CTsPlayer::GetPlayMode()
 int CTsPlayer::SetVideoWindow(int x,int y,int width,int height)
 {
 
-/*
 
-    int fd;
-    const char *path = "/sys/class/video/axis" ;
-    char  bcmd[32];
-	//x /= 1.5;
-	//y /= 1.5;
-	//width /= 1.5;
-	//height /= 1.5;
-    fd = open(path, O_CREAT | O_RDWR | O_TRUNC, 0644);
-    if (fd >= 0) {
-            sprintf(bcmd, "%d %d %d %d", x, y, x+width, y+height);
-            write(fd, bcmd, strlen(bcmd));
-        close(fd);
+   __android_log_print(ANDROID_LOG_INFO, "TsPlayer", "CTsPlayer::SetVideoWindow:  %d, %d ,%d ,%d\n",x ,y ,width,height);
+   
+	int fd_axis,fd_mode;
+	int x2 = 0,y2 = 0,width2 = 0,height2 = 0;
+
+	const char *path_mode = "/sys/class/display/mode" ;
+	const char *path_axis = "/sys/class/video/axis" ;
+
+	char  bcmd[32];
+      
+	char buffer[15];
+	int mode_w = 0,mode_h = 0;
+
+   fd_mode= open(path_mode, O_RDONLY);
+   if(fd_mode >= 0){ 
+    memset(buffer, 0 ,sizeof(buffer));       
+	read(fd_mode,buffer,sizeof(buffer));
+	__android_log_print(ANDROID_LOG_INFO, "TsPlayer", " mode_buffer = %s \n",buffer);
+    
+
+	if(!strncmp(buffer,"1080i50hz",9) || !strncmp(buffer,"1080p50hz",9))
+	{
+		mode_w=1920;
+		mode_h= 1080;
+	}
+	else if(!strncmp(buffer,"720p",4))
+	{
+		mode_w=1280;
+		mode_h=720;
+	}
+	else if(!strncmp(buffer,"567i",4))
+	{
+		mode_w=720;
+		mode_h=567;
+	}
+
+   else if( !strncmp(buffer,"480i",4) )
+	{
+		mode_w=720;
+		mode_h=480;
+	}
+        
+     close(fd_mode);
+       
+	}
+
+
+   __android_log_print(ANDROID_LOG_INFO, "TsPlayer", "CTsPlayer::mode_w = %d , mode_h = %d , mw = %d, mh = %d \n",mode_w,mode_h,m_nEPGWidth,m_nEPGHeight);
+
+   if(m_nEPGWidth !=0 && m_nEPGHeight !=0)
+	{
+		x2=x*mode_w/m_nEPGWidth;
+		width2=width*mode_w/m_nEPGWidth;
+		y2=y*mode_h/m_nEPGHeight;
+		height2=height*mode_h/m_nEPGHeight;
+	}
+
+
+
+	fd_axis = open(path_axis, O_CREAT | O_RDWR | O_TRUNC, 0644);
+
+
+	if (fd_axis >= 0) {
+
+		sprintf(bcmd, "%d %d %d %d", x2, y2, x2+width2, y2+height2);            
+            
+		write(fd_axis, bcmd, strlen(bcmd));
+		__android_log_print(ANDROID_LOG_INFO, "TsPlayer", "setvideoaxis: %s\n", bcmd);
+        close(fd_axis);
+
         return 0;
-    }
-	return -1;*/
+    	}
 
-return 0 ;
+	return 0 ;
 }
 
 
@@ -543,10 +824,6 @@ int CTsPlayer::VideoShow(void)
 int CTsPlayer::VideoHide(void)
 {
 
-
-	if (m_nMode == M_LIVE)
-		return 0;
-	return 0;
 	return set_sys_int("/sys/class/video/disable_video",1);
 }
 
@@ -710,6 +987,7 @@ bool CTsPlayer::Stop()
 	{
 		__android_log_print(ANDROID_LOG_INFO, "TsPlayer", "m_bIsPlay is false");
 	}
+	VideoHide(); 
 	m_bIsPlay = false;
 	if (m_bSetEPGSize){
 		if (m_nEPGWidth == 1280 && m_nEPGHeight == 720)

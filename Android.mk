fc41435529dd85cebe620535f96431b9c2db28e1
bo.cao <bo.cao@amlogic.com>
2012-09-25 17:14:18 +0800

    change lib name, follow CTCC

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_ARM_MODE := arm
-LOCAL_MODULE    := libTsPlayer
+LOCAL_MODULE    := libCTC_MediaProcessor
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := \
